c33de186183073aa3c7dee831f4c7b41a8ba6866
t14g <ottiagos@gmail.com>
2020-02-25 16:20:51 -0300

    Reducers trick

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,9 @@
 import { combineReducers } from 'redux';
 
-export default combineReducers({});
+
+//Trick reducers at start to avoid erros on console 
+//Because you need a valid reducer at start on this combineReducers
+//And you have none at start
+export default combineReducers({
+    trickReducers : () => 'Trick reducers for no erros'
+});
