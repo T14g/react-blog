3aa95c9e54cc1676ed4781d49664fc29a2d2ba44
t14g <ottiagos@gmail.com>
2020-02-29 21:03:15 -0300

    Post reducer

---
+++ src/reducers/postReducer.js
@@ -0,0 +1,11 @@
+//Single file separated reducer
+//You not alloweed to return undefined 
+//Always have to not return undefined from reducer!! so the last return
+export default (state = [], action) => {
+    switch(action.type) {
+        case 'FETCH_POSTS':
+            return action.payload;
+        default:
+            return state;
+    }
+}
