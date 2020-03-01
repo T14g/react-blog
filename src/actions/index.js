b8e9d591b95e953ff9748696e3c05bb356dbee90
t14g <ottiagos@gmail.com>
2020-03-01 14:24:55 -0300

    One time memoize

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';//Memoize, run function only once with unique parameters
 import jsonPlaceHolder from '../apis/jsonPlaceholder';
 
 //Action creator
@@ -11,8 +12,12 @@ export const fetchPosts = () => async dispatch => {
         dispatch({type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = id => async dispatch => {
+export const fetchUser = id => dispatch =>  _fetchUser(id, dispatch);
+
+//Memoize helps at not making the same request for a user with a given ID over and over again
+//it just return the previous value if you call the function again with the same parameters
+const _fetchUser = _.memoize(async (id, dispatch) => {
     const responese = await jsonPlaceHolder.get(`/users/${id}`);
     dispatch({ type: 'FETCH_USER', payload: responese.data });
-}
+});
 
